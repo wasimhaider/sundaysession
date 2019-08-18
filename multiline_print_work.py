cb4c9373c77e401b2703d753bbbf2bba4742c58c
wasim haider <wasim.haider@global.internal>
2019-08-18 12:31:41 +0530

    My sunday session

---
+++ multiline_print_work.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print ('Wasim Haider')
+
+
+# In[39]:
+
+
+name = input('What is your name? ')
+city = input ('Where are you from? ')
+print (''' What is you name and how may help, {} 
+Which city you are from, {}
+''' .format (name, city))
+
